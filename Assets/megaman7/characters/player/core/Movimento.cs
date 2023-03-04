b7932be4ad480db8917bf8a42510b4fe0a8c47ba
wilson melo <bakudas@gmail.com>
2023-03-04 16:30:42 -0300

    player animation and some tweaks

--- Assets/megaman7/characters/player/core/Movimento.cs
+++ Assets/megaman7/characters/player/core/Movimento.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Movimento : MonoBehaviour
@@ -8,19 +10,26 @@ public class Movimento : MonoBehaviour
     // Components
     private PlayerData playerData;
     private Rigidbody2D rb;
+    private Animator _animator; 
     
     // Movement Vars
+    private bool isFirstDrop = true; 
     private bool canMove = false;
     private bool isMoving = false;
     private bool isShooting = false;
     private bool isJumping = false;
     private bool isGrounded = false;
-    
+    private SpriteRenderer _spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        canMove = true;
+        
         playerData = GetComponent<PlayerCore>().Data;
         rb = GetComponentInParent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
         
         Debug.Log(playerData.speed);
         Debug.Log(rb);
@@ -29,6 +38,47 @@ public class Movimento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       isGrounded = true if rb.OverlapCollider() 
+        float moveX = Input.GetAxisRaw("Horizontal") * playerData.speed;
+
+        if (moveX < 0) _spriteRenderer.flipX = false;
+        else if (moveX > 0) _spriteRenderer.flipX = true;
+            
+        if (moveX != 0) isMoving = true;
+        else isMoving = false;
+
+        rb.velocity = new Vector2( moveX, rb.velocity.y);
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("PULEIE");
+            rb.AddForce(Vector2.up * playerData.jumpSpeed);
+            _animator.SetBool("isJumping", true);
+            isGrounded = false;
+        }
+
+        if (rb.velocity.y < 0 && !isFirstDrop)
+        {
+            _animator.SetBool("isFalling", true);
+            _animator.SetBool("isJumping", false);
+        }
+
+        _animator.SetBool("isMoving", isMoving); 
+        _animator.SetBool("isGrounded", isGrounded); 
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject chao = col.gameObject;
+        Debug.Log(chao);
+        if (chao.CompareTag("floor"))
+        {
+            if (isFirstDrop) isFirstDrop = false;
+            Debug.Log("tou no chão bixo");
+            isGrounded = true;
+            _animator.SetBool("isFalling", false);
+            _animator.SetBool("isJumping", false);
+        }
+        
     }
 }
